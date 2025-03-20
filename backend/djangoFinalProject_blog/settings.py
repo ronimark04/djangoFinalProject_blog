a6a7854cc50eed01d80ff56ed5a6be4f12d38bf0
ronimark04 <roni.mark@gmail.com>
2025-03-20 16:19:40 +0200

    cors handled, react app initialized

--- backend/djangoFinalProject_blog/settings.py
+++ backend/djangoFinalProject_blog/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'taggit',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'djangoFinalProject_blog.urls'

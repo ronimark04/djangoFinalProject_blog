0ced94bb0eea1b5d3fa755db78b9e9345852539b
ronimark04 <roni.mark@gmail.com>
2025-03-20 15:02:06 +0200

    pagination

--- djangoFinalProject_blog/settings.py
+++ djangoFinalProject_blog/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication'
